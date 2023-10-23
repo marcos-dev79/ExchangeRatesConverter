2a9b021a844456c8f08f526aea591a3785de3d44
Hector Menezes <hector.menezes@gmail.com>
2023-10-23 01:45:21 -0300

    docs: add simple docs at swagger
    Perform some minor refactors around the code

--- src/main/java/com/wex/test/controllers/PurchaseController.java
+++ src/main/java/com/wex/test/controllers/PurchaseController.java
@@ -5,12 +5,15 @@ import com.wex.test.domainObj.PurchaseTransaction;
 import com.wex.test.domainObj.RequestResponse;
 import com.wex.test.domainObj.SearchRequest;
 import com.wex.test.services.PurchaseService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(path = "purchaseTransaction")
 public class PurchaseController {
@@ -19,16 +22,38 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping(path = "/all")
+    @Tag(
+        name = "Purchase Transaction",
+        description = "Operations on purchase transactions"
+    )
+    @Operation(
+        summary = "Get all operations",
+        description = "Retrieve all operation in the system without any filter or sorting"
+    )
     public List<PurchaseTransaction> getAll() {
         return purchaseService.findAll();
     }
 
     @PostMapping("/save")
+    @Tag(
+        name = "Purchase Transaction"
+    )
+    @Operation(
+        summary = "Save a transaction",
+        description = "Receives a transaction, saves it in the database and return the given object"
+    )
     public PurchaseTransaction save(@RequestBody PurchaseTransaction purchaseTransaction) {
         return purchaseService.save(purchaseTransaction);
     }
 
     @PostMapping("/convert")
+    @Tag(
+        name = "Purchase Transaction"
+    )
+    @Operation(
+        summary = "Convert transaction",
+        description = "Perform the convertion of a given transaction"
+    )
     public RequestResponse convert(@RequestBody SearchRequest searchRequest) throws JsonProcessingException {
         RequestResponse requestResponse = purchaseService.getConvertedData(searchRequest);
         return requestResponse;
--- src/main/java/com/wex/test/repositories/PurchaseTransactionRepository.java
+++ src/main/java/com/wex/test/repositories/PurchaseTransactionRepository.java
@@ -3,14 +3,13 @@ package com.wex.test.repositories;
 
 import com.wex.test.domainObj.PurchaseTransaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@RepositoryRestResource(collectionResourceRel = "purchaseTransaction", path = "purchaseTransaction")
+@Repository
 public interface PurchaseTransactionRepository extends MongoRepository<PurchaseTransaction, String> {
     public List<PurchaseTransaction> findAll();
     public Optional<PurchaseTransaction> findByUuid(String id);
-
 }
